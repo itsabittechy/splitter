9ed07ade17d9bb07056bea6b7311329cd9adc775
itsabittechy <shxrnibills@gmail.com>
2021-11-01 14:39:37 +0000

    changed function to produce random colour blocks

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 "use strict"
 
-alert("hello")
+function split4(){
+    addQuarter(this)
+    addQuarter(this)
+    addQuarter(this)
+    addQuarter(this)
+
+}
+function addQuarter(intoDiv) {
+    let q= document.createElement("div")
+    q.classList.add("flexy")
+    q.classList.add("quarter")
+    
+    q.addEventListener("click",split4)
+    intoDiv.appendChild(q)
+    
+    q.style.backgroundColor=`rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`
+
+}
+document.body.addEventListener("click",split4)
